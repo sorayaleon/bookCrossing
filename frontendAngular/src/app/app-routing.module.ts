98e5996cabbbb561917033d41512871e3c047333
sorayaleon <soraya.l.g.88@gmail.com>
2019-12-05 21:21:52 +0100

    Rutas angular login y registro

--- frontendAngular/src/app/app-routing.module.ts
+++ frontendAngular/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
+import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'login', component: LoginComponent},
+  {path: 'registro', component: RegistroComponent},
+  {path: 'requestPass', component: RequestResetComponent},
+  {path: 'responsePass', component: ResponseResetComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
